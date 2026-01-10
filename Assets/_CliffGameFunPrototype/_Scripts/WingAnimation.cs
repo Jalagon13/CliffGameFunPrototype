b4fa7b90a7b0046b5fb3bd6ab3c068676051c643
Jalagon <A1ag0nJus1in@gmail.com>
2026-01-11 08:26:29 +0900

    Bird model and raw bird dropping from bird done

---
+++ Assets/_CliffGameFunPrototype/_Scripts/WingAnimation.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+namespace CliffGame
+{
+    public class WingAnimation : MonoBehaviour
+    {
+        [SerializeField]
+        private float _maxZRotationDegrees = 30f;
+
+        [SerializeField]
+        private float _cycleDuration = 1f;
+
+        [SerializeField]
+        private bool _startAtPositiveRotation = true;
+
+        private float _timer;
+
+        private void Start()
+        {
+            _timer = _startAtPositiveRotation ? _cycleDuration : 0f;
+        }
+
+        private void Update()
+        {
+            if (_cycleDuration <= 0f)
+                return;
+
+            _timer += Time.deltaTime;
+
+            float t = Mathf.PingPong(_timer, _cycleDuration) / _cycleDuration;
+            float zRotation = Mathf.Lerp(-_maxZRotationDegrees, _maxZRotationDegrees, t);
+
+            Vector3 currentEuler = transform.localEulerAngles;
+            transform.localRotation = Quaternion.Euler(currentEuler.x, currentEuler.y, zRotation);
+        }
+    }
+}

ee09c2fb3c458aa26d12e460a766d43c92ea5dea
Jalagon <A1ag0nJus1in@gmail.com>
2026-04-01 09:32:55 +0900

    Stamina bar removed

--- Assets/_CliffGameFunPrototype/_Scripts/_UI/StatsUI.cs
+++ Assets/_CliffGameFunPrototype/_Scripts/_UI/StatsUI.cs
@@ -10,11 +10,6 @@ namespace CliffGame
         [SerializeField] private MMProgressBar _healthBar;
         [SerializeField] private MMProgressBar _hungerBar;
 
-        [Header("Stamina Settings")]
-        [SerializeField] private MMProgressBar _staminaBar;
-        [SerializeField, Range(0f, 1f)] private float _staminaDropThreshold = 0.2f;
-        [SerializeField] private MMF_Player _staminaDropThresholdFeedback;
-
         private TextMeshProUGUI _healthText;
         private TextMeshProUGUI _hungerText;
         private TextMeshProUGUI _staminaText;
@@ -24,23 +19,19 @@ namespace CliffGame
         {
             _healthText = _healthBar.transform.GetChild(4).GetComponent<TextMeshProUGUI>();
             _hungerText = _hungerBar.transform.GetChild(4).GetComponent<TextMeshProUGUI>();
-            _staminaText = _staminaBar.transform.GetChild(4).GetComponent<TextMeshProUGUI>();
         }
 
         private void Start()
         {
             HealthManager.Instance.OnHealthChanged += HandleHealthChanged;
             HungerManager.Instance.OnHungerChanged += HandleHungerChanged;
-            StaminaManager.Instance.OnStaminaChanged += HandleStaminaChanged;
             Player.Instance.OnStateChanged += OnMoveStateChanged;
-            _staminaBar.HideBar(0f);
         }
 
         private void OnDestroy()
         {
             HealthManager.Instance.OnHealthChanged -= HandleHealthChanged;
             HungerManager.Instance.OnHungerChanged -= HandleHungerChanged;
-            StaminaManager.Instance.OnStaminaChanged -= HandleStaminaChanged;
             Player.Instance.OnStateChanged -= OnMoveStateChanged;
         }
 
@@ -58,26 +49,6 @@ namespace CliffGame
         {
             _hungerBar.UpdateBar(currentAmount, 0, maxAmount);
             _hungerText.text = $"Hunger: {currentAmount}%";
-        }
-
-        private void HandleStaminaChanged(int currentAmount, int maxAmount)
-        {
-            float currentPercent = (float)currentAmount / maxAmount;
-            bool isFull = currentPercent >= 1f;
-
-            if (_previousStaminaPercent > _staminaDropThreshold && currentPercent <= _staminaDropThreshold)
-            {
-                _staminaDropThresholdFeedback.PlayFeedbacks();
-            }
-
-            _previousStaminaPercent = currentPercent;
-            _staminaBar.UpdateBar(currentAmount, 0, maxAmount, false);
-            _staminaText.text = $"Stamina: {Mathf.RoundToInt(currentPercent * 100f)}%";
-
-            if (isFull)
-            {
-                _staminaBar.HideBar(0f);
-            }
         }
     }
 }
